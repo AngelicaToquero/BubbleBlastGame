c6e4ea724bdd456f93796cf9a3cf9b0fe0d45ce5
Angelica Toquero <152822754+AngelicaToquero@users.noreply.github.com>
2024-04-26 12:02:01 +0000

    .

---
+++ Frames/Main.java
@@ -0,0 +1,12 @@
+package Frames;
+
+import javax.swing.SwingUtilities;
+
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            LandingFrame frame= new LandingFrame();
+            frame.setVisible(true);
+        });
+    }
+}
